67f58243789d3ab6bc0787e6b31e5503bc346593
nellyXinwei <nelsonalbajr@yahoo.com>
2021-04-12 11:58:47 +0800

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '245df266575ffa01c4859378c1bef87b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f409627e-7c4c-43bf-a518-306c8c248f31'
+};
